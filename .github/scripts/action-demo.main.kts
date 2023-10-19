6d8c57940f1a8a7f993b7a22121e97c04107d00f
umang91 <umangchamaria@gmail.com>
2023-10-19 23:51:28 +0530

    test error handling

--- .github/scripts/action-demo.main.kts
+++ .github/scripts/action-demo.main.kts
@@ -1,11 +1,16 @@
 #!/usr/bin/env kotlin
 
-fun executeCommandOnShell(command: String) {
+fun executeCommandOnShell(command: String): Int {
     val process = ProcessBuilder("/bin/bash", "-c", command).inheritIO().redirectErrorStream(true)
         .start()
     process.waitFor()
+    return process.exitValue()
 }
 
-executeCommandOnShell("./gradlew assemble")
-executeCommandOnShell("./gradlew ktlintCheck")
-executeCommandOnShell("./gradlew test")
+var exitValue = executeCommandOnShell("./gradlew assemble")
+//println("assemble exit value - $exitValue")
+exitValue = executeCommandOnShell("./gradlew ktlintCheck")
+//println("ktlintCheck exit value - $exitValue")
+exitValue = executeCommandOnShell("./gradlew test")
+//println("unit test exit value $exitValue")
+if (exitValue != 0) Runtime.getRuntime().exit(1)
