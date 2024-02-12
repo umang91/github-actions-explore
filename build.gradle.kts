0d6eadfa5e4a34339279b1c3d00f2e3874714fb4
umang91 <umangchamaria@gmail.com>
2024-02-12 23:00:21 +0530

    remove kt lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
     id("com.android.library") version ("8.0.2") apply false
     id("org.jetbrains.kotlin.android") version ("1.7.20") apply false
     id("org.jetbrains.dokka") version ("1.8.20") apply false
-    id("org.jlleitschuh.gradle.ktlint") version ("11.2.0")
+//    id("org.jlleitschuh.gradle.ktlint") version ("11.2.0")
     id("io.codearte.nexus-staging") version ("0.30.0")
 }
 
 subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+//    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
