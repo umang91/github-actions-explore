f94b22c99edda8dd00df56d811f7c7ef265f677a
umang91 <umangchamaria@gmail.com>
2024-10-19 13:34:59 +0530

    update run command

--- .github/scripts/input-demo.main.kts
+++ .github/scripts/input-demo.main.kts
@@ -1,7 +1,7 @@
 #!/usr/bin/env kotlin
 
 val inputValueOne = args[0]
-val inputValueTwo = args[1]
+// val inputValueTwo = args[1]
 
 println("input value 1: $inputValueOne")
-println("input value 2: $inputValueTwo")
+// println("input value 2: $inputValueTwo")
